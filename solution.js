5860f436688b0c678d05e00bb73eb12a092466bb
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-12 16:47:17 -0600

    Add exercise typeof

---
+++ solution.js
@@ -0,0 +1,7 @@
+function findType(valor) {
+    return typeof (valor);
+}
+
+console.log(findType(true));
+console.log(findType("Hello"));
+console.log(findType(1.5));
---
+++ typeof.js
@@ -0,0 +1,15 @@
+/* In this challenge you will find a function called solution that receives a parameter called value. You must find the data type of the parameter value and return it from the solution function.
+
+Remember that the value parameter will be different for each different way we execute the solution function.
+
+For example:
+
+Given the following calls to the solution function: */
+
+function findType(valor) {
+    // write answer here
+}
+
+console.log(findType(true));
+console.log(findType("Hello"));
+console.log(findType(1.5));
