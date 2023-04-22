60f32c4c591ce1257e5da598c7d25e0db8ce5b7a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-21 19:05:30 -0600

    Add conditions for all inputs

--- script.js
+++ script.js
@@ -4,7 +4,43 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submit', function(e) {
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+// Show success outline
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
 
-    
+// Event Listeners
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+  
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === '') {
+    showError(password2, 'Password 2 is required');
+  } else {
+    showSuccess(password2);
+  }
 });
