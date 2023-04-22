7e9cb72a70c4dfd9e7ae059870db918512ecdbf9
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-21 18:26:32 -0600

    Add event listener to fomr

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const input = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', function(e) {
+
+});
